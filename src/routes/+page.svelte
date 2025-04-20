fdd351945d8e3e3cf1cb1c64a185e04c1d8ceac5
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-04-20 19:54:44 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,17 @@
   <title>{title} | {subtitle}</title>
 </svelte:head>
 
-<img
-  src="/lugo_studio_cat_pure_borderless.png"
-  alt="Lugo Studio"
-  class="mx-auto flex max-w-sm items-center drop-shadow-xl/100"
-/>
+<div class="mx-auto max-w-full lg:max-w-1/2 h-screen text-ctp-text">
+  <div class="grid grid-cols-1 gap-20">
+    <div class="mx-auto relative">
+      <img
+        src="/lugo_studio_cat_pure_borderless.png"
+        alt="Lugo Studio"
+        class="drop-shadow-xl/100"
+      />
+    </div>
+    <div class="justify-self-center-safe">
+      <p>Bau</p>
+    </div>
+  </div>
+</div>
