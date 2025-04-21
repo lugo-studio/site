ae394e0103ce0d2772e2abd9a778bf9500b40e06
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-04-21 02:28:07 -0400

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -12,7 +12,7 @@
     <Logo subtitle="Error" />
     <Sep />
     <div class="grid grid-cols-1 gap-10">
-      <div class="text-5xl text-center">Ope, something went wrong.</div>
+      <div class="text-5xl text-center">Oh nyo~!</div>
       <SepLabel label={page.error?.message} width={"1/4"} />
     </div>
   </div>
