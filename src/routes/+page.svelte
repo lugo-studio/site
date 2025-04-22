43bf6227fa2c47c1f2a07eb40948a012a4571dc1
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-04-21 23:24:13 -0400

    Icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,5 +9,13 @@
 {/snippet}
 
 {#snippet content()}
+  <div class="columns-auto gap-10 text-center">
+    <a href="https://github.com/Lugo-Studio" aria-label="GitHub">
+      <span class="text-8xl icon-[octicon--mark-github-24]"></span>
+    </a>
+    <a href="https://x.com/GTLugo" aria-label="Twitter (X)">
+      <span class="text-8xl icon-[line-md--twitter-x]"></span>
+    </a>
+  </div>
   <div class="px-8"></div>
 {/snippet}
