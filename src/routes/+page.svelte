62fb5d89f1102ae2c43708a8eca3beb25c631093
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-04-21 23:46:19 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,16 +5,31 @@
 <Page delimiter="" subtitle="" {header} {content} />
 
 {#snippet header()}
-  <div class="text-5xl text-center">Hello hello, friends.</div>
+  <div class="text-5xl text-center text-ctp-subtext0">Hello hello, friends.</div>
 {/snippet}
 
 {#snippet content()}
   <div class="columns-auto gap-10 text-center">
-    <a href="https://github.com/Lugo-Studio" aria-label="GitHub">
-      <span class="text-8xl icon-[octicon--mark-github-24]"></span>
+    <a
+      href="https://github.com/Lugo-Studio"
+      aria-label="GitHub"
+      class="text-ctp-text"
+    >
+      <span class="text-8xl icon-[line-md--github]"></span>
     </a>
-    <a href="https://x.com/GTLugo" aria-label="Twitter (X)">
+    <a
+      href="https://x.com/GTLugo"
+      aria-label="Twitter (X)"
+      class="text-ctp-text"
+    >
       <span class="text-8xl icon-[line-md--twitter-x]"></span>
+    </a>
+    <a
+      href="https://www.linkedin.com/in/gabriel-a-lugo/"
+      aria-label="LinkedIn"
+      class="text-ctp-text"
+    >
+      <span class="text-8xl icon-[line-md--linkedin]"></span>
     </a>
   </div>
   <div class="px-8"></div>
