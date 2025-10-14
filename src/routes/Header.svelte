b89238fab44ba711523bd54ff568c24fc5e50c71
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-10-14 09:11:17 -0400

    Update NavUl props to remove deprecated usage

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -5,7 +5,10 @@
 <header class="md:pt-4 md:px-4">
   <Navbar class="rounded-lg bg-ctp-crust!">
     <NavHamburger />
-    <NavUl class="mx-auto bg-ctp-crust!" ulClass="rounded-lg bg-ctp-crust!">
+    <NavUl
+      class="mx-auto bg-ctp-crust!"
+      classes={{ ul: "rounded-lg bg-ctp-crust!" }}
+    >
       <NavLi href="/">Home</NavLi>
       <NavLi href="/">About</NavLi>
       <NavLi href="https://gtlugo.com">Blog</NavLi>
