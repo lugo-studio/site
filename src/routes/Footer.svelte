a4a6f971e99e31b00b27b60f646f5539af544ed0
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-10-14 17:12:40 -0400

    Header & footer redesign

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -12,15 +12,13 @@
   } from "flowbite-svelte-icons";
 </script>
 
-<footer class="md:pb-4 md:px-4">
-  <Footer class="bg-ctp-crust!">
+<footer class="md:pb-6 md:px-4">
+  <Footer class="bg-transparent! max-md:text-center!">
     <FooterCopyright href="https://lugo.studio/" by="Lugo Studio" year={2025} />
     <FooterLinkGroup
-      class="mt-3 flex flex-wrap items-center text-sm text-ctp-text sm:mt-0"
+      class="mt-0 flex flex-wrap justify-center text-sm text-ctp-text"
     >
-      <div
-        class="mt-4 flex space-x-6 sm:justify-center md:mt-0 rtl:space-x-reverse"
-      >
+      <div class="mt-4 md:mt-0 flex space-x-8 rtl:space-x-reverse">
         <FooterIcon href="https://github.com/Lugo-Studio">
           <GithubSolid
             class="h-5 w-5 text-ctp-text hover:text-gray-900 dark:hover:text-white"
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,17 +1,33 @@
 <script lang="ts">
-  import { Navbar, NavHamburger, NavLi, NavUl } from "flowbite-svelte";
+  import {
+    Navbar,
+    NavBrand,
+    NavHamburger,
+    NavLi,
+    NavUl,
+  } from "flowbite-svelte";
 </script>
 
 <header class="md:pt-4 md:px-4">
-  <Navbar class="rounded-lg bg-ctp-crust!">
+  <Navbar class="rounded-lg">
+    <NavBrand href="/">
+      <img
+        src="/assets/favicon.png"
+        class="me-3 h-6 sm:h-9"
+        alt="Flowbite Logo"
+      />
+    </NavBrand>
     <NavHamburger />
-    <NavUl
-      class="mx-auto bg-ctp-crust!"
-      classes={{ ul: "rounded-lg bg-ctp-crust!" }}
-    >
-      <NavLi href="/">Home</NavLi>
-      <NavLi href="/">About</NavLi>
-      <NavLi href="https://gtlugo.com">Blog</NavLi>
+    <NavUl classes={{ ul: "rounded-lg bg-ctp-crust! md:bg-transparent!" }}>
+      <!-- <NavLi href="/">
+        <span class="md:text-2xl font-[Oxanium] font-bold">Home</span>
+      </NavLi>
+      <NavLi href="/">
+        <span class="md:text-2xl font-[Oxanium] font-bold">About</span>
+      </NavLi> -->
+      <NavLi href="https://gtlugo.com">
+        <span class="md:text-2xl font-[Oxanium] font-bold">Blog</span>
+      </NavLi>
     </NavUl>
   </Navbar>
 </header>
